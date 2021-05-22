e676dee56d839e460150e0c850210537b82ffa00
PHIVEE\obena <aresgames14@gmail.com>
2021-05-22 14:03:20 +0800

    Initial Commit

---
+++ API.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace API.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ DataAccess/Attributes/ColumnAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DataAccess.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class ColumnAttribute:Attribute
+    {
+        public string Name { get; set; }
+    }
+}
